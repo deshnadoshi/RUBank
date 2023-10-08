4eb5c46ba9d3a58ee4927e2b28b444c9d9e9a868
Deshna Doshi <deshna.doshi@gmail.com>
2023-10-07 22:34:37 -0400

    Setting up all the classes

--- src/rutgersbanking/Profile.java
+++ src/rutgersbanking/Profile.java
@@ -4,4 +4,43 @@ public class Profile implements Comparable<Profile> {
     private String fname;
     private String lname;
     private Date dob;
+
+    private final int NOT_EQUAL = -2;
+    public Profile(String fname, String lname, Date dob){
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+    }
+
+    public boolean isValidDOB(){
+       return dob.isValid();
+    }
+
+    @Override
+    public int compareTo(Profile compareProfile){
+        int compareResult = 0;
+
+        int fNameCompareResult = fname.compareTo(compareProfile.getFname());
+        int lNameCompareResult = lname.compareTo(compareProfile.getLname());
+        int dobCompareResult = dob.compareTo(compareProfile.getDOB());
+
+        if (fNameCompareResult == 0 && lNameCompareResult == 0 && dobCompareResult == 0){
+            return compareResult;
+        }
+
+        return NOT_EQUAL;
+    }
+
+    public String getFname(){
+        return fname;
+    }
+
+    public String getLname(){
+        return lname;
+    }
+
+    public Date getDOB(){
+        return dob;
+    }
+
 }
--- src/rutgersbanking/Savings.java
+++ src/rutgersbanking/Savings.java
@@ -1,7 +1,41 @@
 package rutgersbanking;
 
 public class Savings extends Account {
-    protected boolean isLoyal; // loyal customer status
-    private static final int INTEREST_RATE = 0;
-    private static final int FEE = 0;
+    protected boolean isLoyal;
+    private static final double INTEREST_RATE = 0.04;
+    private static final int FEE = 25;
+
+    /**
+     * Constructor to initialize the instance variable.
+     * @param isLoyal Customer loyalty metric.
+     */
+    public Savings(boolean isLoyal){
+        this.isLoyal = isLoyal;
+    }
+
+    // MoneyMarket.java keeps on yelling at me whenever i don't have a default constructor for some reason so i've added one in
+    // i'm not fully sure what it's supposed to be fore though
+    public Savings(){
+
+    }
+
+    /**
+     * Provides the monthly fee associated with the account.
+     * @return the monthly fee.
+     */
+    public double monthlyFee(){
+        return FEE;
+    }
+
+    /**
+     * Provides the monthly interest rate associated with the account.
+     * @return the monthly interest rate.
+     */
+    public double monthlyInterest(){
+        return INTEREST_RATE;
+    }
+
+    public int compareTo(Account compareSavings){
+        return -1;
+    }
 }
