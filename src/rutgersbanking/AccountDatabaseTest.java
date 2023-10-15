cdc4b82e62533f9c603ab48faae284dd96407a80
Deshna Doshi <deshna.doshi@gmail.com>
2023-10-15 18:09:02 -0400

    Added JUnit tests for Date, ActDB (need to fix).

---
+++ src/rutgersbanking/AccountDatabaseTest.java
@@ -0,0 +1,49 @@
+package rutgersbanking;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AccountDatabaseTest {
+
+    /**
+     * Confirms that an account that exists can be closed.
+     */
+    @Test
+    public void test_CloseExistingAccount() {
+
+        Profile deshna = new Profile("Deshna", "Doshi", new Date(2003, 11, 5));
+        Profile haejin = new Profile("Haejin", "Song", new Date(2003, 10, 10));
+
+        Account checking = new Checking(deshna, 2500);
+        Account savings = new Savings(haejin, 3500, true);
+
+        Account [] accounts = {checking, savings};
+
+        AccountDatabase database = new AccountDatabase(accounts, 2);
+        database.open(checking);
+        database.open(savings);
+
+        assertTrue(database.close(checking));
+
+    }
+
+    /**
+     * Confirms that an account that doesn't exist cannot be closed.
+     */
+    @Test
+    public void test_CloseNonExistentAccount(){
+
+        Profile deshna = new Profile("Deshna", "Doshi", new Date(2003, 11, 5));
+        Profile haejin = new Profile("Haejin", "Song", new Date(2003, 10, 10));
+
+        Account checking = new Checking(deshna, 2500);
+        Account savings = new Savings(haejin, 3500, true);
+
+        Account [] accounts = {checking, savings};
+
+        AccountDatabase database = new AccountDatabase(accounts, 2);
+        database.open(savings);
+
+        assertFalse(database.close(checking));
+
+    }
+}
