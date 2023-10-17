91a6bc66b73ad91ea0c247b21a7023fe279adf0b
Deshna Doshi <deshna.doshi@gmail.com>
2023-10-16 23:32:11 -0400

    final

--- src/rutgersbanking/RunProject2.java
+++ src/rutgersbanking/RunProject2.java
@@ -1,5 +1,9 @@
 package rutgersbanking;
 
+/**
+ * Interface to run project.
+ * @author Lily Chang
+ */
 public class RunProject2 {
     public static void main(String[] args) {
         new TransactionManager().run();
